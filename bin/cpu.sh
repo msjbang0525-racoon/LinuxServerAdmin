8d829bd06ed3c2c821d7b7586016249454cfde31
msjbang0525-racoon <msjbang0525@gmail.com>
2026-04-29 18:11:51 +0900

    2nd commit

---
+++ bin/cpu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "+------------------------------------------+"
+echo "| Control-C will terminate cpu.sh process. |"
+echo "+------------------------------------------+"
+
+START=0
+END=1000000000000000000
+while [ $START -le $END ]
+do
+        START=`expr $START + 1`
+done
+
+chmod 700 /root/bin*.sh
